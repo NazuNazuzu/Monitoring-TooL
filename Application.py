8abb0dac1a4219bd22d5f6ccf1b94a280473effb
Jun Kojima <luxeno103@gmail.com>
2023-03-14 20:04:21 +0900

    Tab追加

--- Application.py
+++ Application.py
@@ -1,5 +1,6 @@
 import os
 import tkinter as tk
+import tkinter.ttk as ttk
 
 from dotenv import load_dotenv
 
@@ -11,17 +12,40 @@ class Application(tk.Frame):
             master,
             borderwidth=5,
             relief='groove')
+        env = ENV()
+        self.name = env.name
+        self.email = env.email
+        self.pwd = env.pwd
+        self.login_url = env.login_url
+        self.home_url = env.home_url
+        self.sleep = env.sleep
+        self.version = env.version
+        self.line_token = env.line_token
 
-        e = ENV()
-        print(e.name)
-        print(e.email)
-        print(e.pwd)
-        print(e.login_url)
-        print(e.home_url)
-        print(e.sleep)
-        print(e.version)
-        print(e.line_token)
+        self.create_widgets()
 
+    def create_widgets(self):
+        ## -- Note Frames -- ##
+        note = ttk.Notebook(self.master, width=w, height=300)
+
+        self.bottom_frame = ttk.Frame(master=self.master)
+        self.bottom_frame.pack(side='bottom', fill='x', expand=False)
+
+        # tab一覧
+        main_tab = ttk.Frame(note)
+        register_creator_tab = ttk.Frame(note)
+        register_time_tab = ttk.Frame(note)
+
+        # Frame pack
+        main_tab.pack()
+        register_creator_tab.pack()
+        register_time_tab.pack()
+
+        # tabに追加
+        note.add(main_tab, text='       　　監視　　       ')
+        note.add(register_creator_tab, text='     　　管理番号　　     ')
+        note.add(register_time_tab, text='     　　メモ　　     ')
+        note.pack()
 
 
 class ENV:
